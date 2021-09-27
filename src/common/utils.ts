4e91950e571cc68cc0c09104d825fd571276e611
Stephan Schober <teh.fonsi@gmail.com>
2021-09-27 22:58:22 +0200

    build: add moment js to parse dates

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,7 +1,7 @@
+import moment from 'moment';
+
 export const formateReportDate = (date: Date): string => {
-  return `${date.getFullYear()}-${
-    date.getMonth() + 1
-  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+  return moment(date).format('YYYY-MM-DD HH:mm');
 };
 
 /**
