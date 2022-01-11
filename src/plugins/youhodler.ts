09863f442759c6add6121ea0a2a0cf488f2212d7
Stephan Schober <teh.fonsi@gmail.com>
2022-01-11 22:24:59 +0100

    fix: change Youhodler to new import format

--- src/plugins/youhodler.ts
+++ src/plugins/youhodler.ts
@@ -13,7 +13,8 @@ export default class Youhodler extends Plugin {
       return Promise.resolve(null);
     }
     const dateString = line[10];
-    const date = moment(dateString, 'M/D/YY H:m').toDate(); //5/22/21 19:43
+    // const date = moment(dateString, 'M/D/YY H:m').toDate(); //5/22/21 19:43
+    const date = new Date(Date.parse(dateString));
     const coin = line[5].toUpperCase();
     const amount = parseFloat(line[7]);
     const price = await this._api.getPrice(coin, date);
@@ -38,16 +39,27 @@ export default class Youhodler extends Plugin {
     if (input.includes('Savings earned')) {
       return TRANSACTION_TYPE.LENDING;
     }
+    if (input.includes('SAVING_EARN')) {
+      return TRANSACTION_TYPE.LENDING;
+    }
     if (input.includes('Withdrawal')) {
+      return TRANSACTION_TYPE.WITHDRAW;
+    }
+    if (input.includes('WITHDRAWAL')) {
       return TRANSACTION_TYPE.WITHDRAW;
     }
     if (input.includes('Deposit')) {
       return TRANSACTION_TYPE.DEPOSIT;
     }
-    // TODO: info not in export (yet)
-    // if (input.includes('Exchange')) {
-    //   return TRANSACTION_TYPE.TRADING;
-    // }
+    if (input.includes('DEPOSIT')) {
+      return TRANSACTION_TYPE.DEPOSIT;
+    }
+    // TODO: info not in export (yet), return unkown
+    if (input.includes('EXCHANGE')) {
+      console.log('Youhodler: Found trade but info to which currency not in report yet');
+      console.log('--> Please add manually to other trades');
+      return TRANSACTION_TYPE.UNKNOWN;
+    }
     return TRANSACTION_TYPE.UNKNOWN;
   }
 }
